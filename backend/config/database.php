a1a8f7c7a11f71502dd0e0e39df91c620f4f3639
xlooser404 <shanromesh7@outlook.com>
2025-05-24 21:59:39 +0530

    remove the comented code

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,34 +1,9 @@
-<!-- ?php
-// class Database {
-//     private $host = 'localhost';
-//     private $db_name = 'thenuka_db';
-//     private $username = 'root';
-//     private $password = 'Bavi@2005@1234';
-//     private $conn;
-
-//     public function connect() {
-//         $this->conn = null;
-
-//         try {
-//             $this->conn = new PDO(
-//                 'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-//                 $this->username,
-//                 $this->password
-//             );
-//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//         } catch(PDOException $e) {
-//             echo 'Connection Error: ' . $e->getMessage();
-//         }
-
-//         return $this->conn;
-//     }
-// }
-?> --><?php
+<?php
 class Database {
     private $host = 'localhost';
     private $db_name = 'thenuka_db';
     private $username = 'root';
-    private $password = 'Bavi@2005@1234'; // Consider storing in a .env file
+    private $password = ''; // Consider storing in a .env file
     private $conn;
 
     public function connect() {
